286123abbb1109c691250dbef1fc576b31464112
Junbo-Chen <junbochen@hotmail.com>
2025-03-05 15:59:01 +0100

    test

--- EnviromentProject/Program.cs
+++ EnviromentProject/Program.cs
@@ -10,7 +10,7 @@ namespace EnviromentProject
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-
+            //test
             // Add services to the container.
 
             builder.Services.AddControllers();
